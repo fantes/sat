83724fd1627bfb8d5bde1fd4a3d1a4ae68b8ca35
Guillaume Infantes <guillaume.infantes@jolibrain.com>
2021-06-16 16:36:55 +0200

    cleanup

--- models/graphcnnsat.py
+++ models/graphcnnsat.py
@@ -1,4 +1,3 @@
-from torch.cuda.amp import autocast, GradScaler
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -17,12 +16,11 @@ import scipy.sparse
 
 class GraphCNNSAT(nn.Module):
 
-    def __init__(self, num_layers=10, num_mlp_layers=2,  hidden_dim=2, output_dim=2, final_dropout=0.5, random = 0, maxclause = 10, maxvar = 20, graph_type = "var.var", var_classification = True, clause_classification = False, graph_embedding = False, PGSO=False, mPGSO=False, graph_norm = True, neighbor_pooling_type = "sum", graph_pooling_type = "average", lfa = True,  device=torch.device("cuda:0")):
+    def __init__(self, num_layers=10, num_mlp_layers=2,  hidden_dim=2, output_dim=2, final_dropout=0.5, random = 1, maxclause = 10, maxvar = 20, graph_type = "var.var", var_classification = True, clause_classification = False, graph_embedding = False, PGSO=False, mPGSO=False, graph_norm = True, neighbor_pooling_type = "sum", graph_pooling_type = "average", lfa = True):
 
         super(GraphCNNSAT, self).__init__()
 
         self.final_dropout = final_dropout
-        self.device = device
         self.num_layers = num_layers
         self.random = random
         self.input_dim = 1 # arities
@@ -44,7 +42,6 @@ class GraphCNNSAT(nn.Module):
         else:
             raise Exception("unkown graph type")
 
-
         self.lfa = lfa #last fully adjacent layer
         if self.varvar:
             self.maxclause = 0
@@ -61,7 +58,7 @@ class GraphCNNSAT(nn.Module):
         self.graph_pooling_type = graph_pooling_type
 
         self.eps = nn.Parameter(torch.ones(self.num_layers-1))
-        if self.mPGSO:
+        if mPGSO:
             self.a = nn.Parameter(torch.ones(self.num_layers-1))
             self.m1 = nn.Parameter(torch.ones(self.num_layers-1))
             self.m2 = nn.Parameter(torch.ones(self.num_layers-1))
@@ -69,8 +66,7 @@ class GraphCNNSAT(nn.Module):
             self.e1 = nn.Parameter(torch.ones(self.num_layers-1))
             self.e2 = nn.Parameter(torch.ones(self.num_layers-1))
             self.e3 = nn.Parameter(torch.ones(self.num_layers-1))
-            self.PGSO = True
-        elif self.PGSO:
+        elif PGSO:
             self.a = nn.Parameter(torch.ones(1))
             self.m1 = nn.Parameter(torch.ones(1))
             self.m2 = nn.Parameter(torch.ones(1))
@@ -78,7 +74,6 @@ class GraphCNNSAT(nn.Module):
             self.e1 = nn.Parameter(torch.ones(1))
             self.e2 = nn.Parameter(torch.ones(1))
             self.e3 = nn.Parameter(torch.ones(1))
-
 
         self.mlps = torch.nn.ModuleList()
         self.norms = torch.nn.ModuleList()
@@ -96,9 +91,6 @@ class GraphCNNSAT(nn.Module):
             else:
                 self.norms.append(nn.BatchNorm1d(self.hidden_dim))
 
-        self.mlps.to(self.device)
-        self.norms.to(self.device)
-
         self.var_classification = var_classification
 
         self.linears_prediction = torch.nn.ModuleList()
@@ -107,15 +99,15 @@ class GraphCNNSAT(nn.Module):
                 self.linears_prediction.append(nn.Linear(self.input_dim, self.output_dim))
             else:
                 self.linears_prediction.append(nn.Linear(self.hidden_dim, self.output_dim))
-        self.linears_prediction.to(self.device)
+        self.linears_prediction
 
-        self.fc1 = nn.Linear(self.hidden_dim, self.output_dim).to(device)
+        self.fc1 = nn.Linear(self.hidden_dim, self.output_dim)
         self.final_dropout = final_dropout
 
         if self.lfa:
-            self.var_lfa = nn.Linear(self.maxvar, self.maxvar).to(device)
+            self.var_lfa = nn.Linear(self.maxvar, self.maxvar)
             if not varvar:
-                self.clause_lfa = nn.Linear(self.maxclause, self.maxclause).to(device)
+                self.clause_lfa = nn.Linear(self.maxclause, self.maxclause)
 
 
     def print_mem(self, name, var):
@@ -130,20 +122,18 @@ class GraphCNNSAT(nn.Module):
 
         if self.neighbor_pooling_type == "average" or self.PGSO:
             if self.varvar:
-                degrees = torch.sparse.mm(biggraph, torch.ones((biggraph.shape[0], 1)).to(self.device)).to(torch.short)
+                degrees = torch.sparse.mm(biggraph, torch.ones((biggraph.shape[0], 1)).to(h_var.device)).to(torch.short)
             else:
-                tempones = torch.ones((biggraph.shape[1], 1)).to(self.device)
+                tempones = torch.ones((biggraph.shape[1], 1),device=h_var.device)
                 degree_clauses = torch.sparse.mm(biggraph, tempones).to(torch.short)
-                tempones = torch.ones((biggraph.shape[0], 1)).to(self.device)
+                tempones = torch.ones((biggraph.shape[0], 1),device=h_var.device)
                 degree_vars = torch.sparse.mm(torch.transpose(biggraph,0,1), tempones).to(torch.short)
                 degrees = torch.cat([degree_clauses, degree_vars])
 
-        # how to weight efficiently every param in hspmm sum
-
-        if self.PGSO:
-            lindex = 0
         if self.mPGSO:
             lindex = layer
+        elif self.PGSO:
+            lindex = 0
         if self.PGSO or self.mPGSO:
             la = self.a[lindex]
             le1 = self.e1[lindex]
@@ -157,23 +147,21 @@ class GraphCNNSAT(nn.Module):
         if not self.varvar: #ie graph is clause x var
 
             if self.PGSO:
-                dje3 = torch.pow(degree_vars.to_dense() * la,le3)
+                dje3 = torch.pow(degree_vars * la,le3)
                 h_var_dje3 = torch.mul(h_var,dje3)
-                clause_pooled = torch.sparse.mm(biggraph, h_var_dje3)
-                die2 = torch.pow(degree_clauses,le2)
+                clause_pooled = torch.sparse.mm(biggraph, h_var_dje3.to(torch.float))
+                die2 = torch.pow(degree_clauses,le2.to(torch.float))
                 clause_pooled = torch.mul(clause_pooled, die2)
 
-                dje3 = torch.pow(degree_clauses,le3)
+                dje3 = torch.pow(degree_clauses,le3.to(torch.float))
                 h_clause_dje3 = torch.mul(h_clause,dje3)
-                var_pooled = torch.sparse.mm(torch.transpose(biggraph,0,1), h_clause_dje3)
-                die2 = torch.pow(degree_vars * la ,le2)
+                var_pooled = torch.sparse.mm(torch.transpose(biggraph,0,1), h_clause_dje3.to(torch.float))
+                die2 = torch.pow(degree_vars * la ,le2.to(torch.float))
                 var_pooled = torch.mul(var_pooled, die2)
 
             else:
                 clause_pooled = torch.sparse.mm(biggraph, h_var.to(torch.float)).to(torch.half)
                 var_pooled = torch.sparse.mm(torch.transpose(biggraph,0,1), h_clause.to(torch.float)).to(torch.half)
-
-
 
             if self.neighbor_pooling_type == "average": #should be subsumed by PGSO
                 clause_pooled = clause_pooled/degree_clauses.expand_as(clause_pooled)
@@ -195,18 +183,17 @@ class GraphCNNSAT(nn.Module):
             if self.neighbor_pooling_type == "average": #should be subsumed by graph_shift
                 pooled = pooled/degree.expand_as(pooled)
 
-
         if self.varvar:
             h = h_var
         else:
             h = torch.cat([h_clause, h_var])
 
         if self.PGSO:
-            dai_e1 = torch.pow(degrees, le1)
+            dai_e1 = torch.pow(degrees, le1.to(torch.float))
             pooled = (dai_e1 * lm1 * h) + lm3 * h + lm2 * pooled
         else:
             pooled +=  (1+self.eps[layer])*h
-        pooled = F.relu(self.norms[layer](self.mlps[layer](pooled)))
+        pooled = F.relu(self.norms[layer](self.mlps[layer](pooled.to(torch.half))))
 
         if self.varvar:
             return None, pooled
@@ -219,10 +206,10 @@ class GraphCNNSAT(nn.Module):
         if self.random > 0:
             for r in range(self.random):
                 if not self.varvar:
-                    r1 = torch.randint(self.random, size=(len(clause_feat),1)).half() / self.random
-                    clause_feat = torch.cat([clause_feat, r1],1).to(self.device)
-                r2 = torch.randint(self.random, size=(len(var_feat), 1)).half() / self.random
-                var_feat = torch.cat([var_feat, r2],1).to(self.device)
+                    r1 = torch.randint(self.random, size=(len(clause_feat),1),device=var_feat.device).half() / self.random
+                    clause_feat = torch.cat([clause_feat, r1],1)
+                r2 = torch.randint(self.random, size=(len(var_feat), 1),device=var_feat.device).half() / self.random
+                var_feat = torch.cat([var_feat, r2],1)
 
         if self.graph_embedding:
             if not self.varvar:
@@ -232,13 +219,13 @@ class GraphCNNSAT(nn.Module):
         if self.varvar:
             h_clause = None
         else:
-            h_clause = clause_feat.to(self.device)
-        h_var = var_feat.to(self.device)
-
+            h_clause = clause_feat
+        h_var = var_feat
 
         for layer in range(self.num_layers-1):
             print(layer)
             h_clause, h_var  = self.next_layer_eps(h_clause,h_var, layer, biggraph,batch_size)
+            print(layer)
             if self.graph_embedding:
                 if not self.varvar:
                     clause_hidden_rep.append(h_clause)
@@ -254,13 +241,10 @@ class GraphCNNSAT(nn.Module):
                 batched_clause = torch.reshape(h_var,(batch_size, self.maxclause, -1))
                 lin_clause = self.clause_lfa(torch.transpose(batched_clause,1,2))
                 h_clause = torch.reshape(torch.transpose(lin_clause, 1,2),(batch_size*self.maxclause,-1))
-
             if self.graph_embedding:
                 if not self.varvar:
                     clause_hidden_rep.append(h_clause)
                 var_hidden_rep.append(h_var)
-
-
 
         if self.var_classification:
             if self.clause_classification:
@@ -268,7 +252,6 @@ class GraphCNNSAT(nn.Module):
             return torch.softmax(self.fc1(h_var), 1)
         if self.clause_classification:
             return torch.softmax(self.fc1(h_clause), 1)
-
 
         #below graph_embedding only
         score_over_layer = 0
@@ -291,7 +274,7 @@ class GraphCNNSAT(nn.Module):
 
 def main():
     varvar = False
-    model = GraphCNNSAT(var_classification=True, clause_classification=False, graph_embedding = False, mPGSO=False, maxclause = 20000000, maxvar = 5000000, lfa = False, graph_norm = True, num_layers=5, graph_type="clause.var")
+    model = GraphCNNSAT(var_classification=True, clause_classification=False, graph_embedding = False, mPGSO=True, maxclause = 20000000, maxvar = 5000000, lfa = False, graph_norm = True, num_layers=5, graph_type="clause.var")
     model.half()
     tds = GraphDataset('../data/test_clausevar_negaslink/T102.2.1.graph', cachesize=0, path_prefix="/home/infantes/code/sat/data/", neg_as_link=True)
     m,labels = tds.getitem(0)
@@ -299,24 +282,28 @@ def main():
 
     batch_size = len(batch_graph)
 
-
-
     clause_feat, var_feat, nclause, nvar = get_feat(batch_graph, varvar, model.maxclause, model.maxvar, dtype=torch.half)
 
+    device=torch.device("cuda:0")
+    clause_feat = clause_feat.to(device)
+    var_feat = var_feat.to(device)
+
     #TODO : precompute degrees
-    biggraph = big_tensor_from_batch_graph(batch_graph,varvar, model.maxclause,model.maxvar,neg_as_link=True).to(model.device).to(torch.float)
+    biggraph = big_tensor_from_batch_graph(batch_graph,varvar, model.maxclause,model.maxvar,neg_as_link=True).to(torch.float).to(device)
 
-
+    model.to(device)
 
     opt = torch.optim.SGD(model.parameters(), lr=1e-3)
-    start  = time.process_time()
 
+    start  = time.process_time()
     p = model.forward(batch_size, biggraph, clause_feat, var_feat, None)
     print("forward time " +str(time.process_time()-start))
     loss_fn = nn.L1Loss()
     loss = loss_fn(p,torch.zeros_like(p))
 
+    start  = time.process_time()
     loss.backward()
+    print("backward time " +str(time.process_time()-start))
     opt.step()
     opt.zero_grad()
 
