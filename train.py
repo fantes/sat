e92564e8c577242dcdf340079a3eb9ca4f337c01
Guillaume Infantes <guillaume.infantes@jolibrain.com>
2021-06-30 13:39:52 +0200

    cleaup

--- train.py
+++ train.py
@@ -3,18 +3,20 @@ from models.graphcnnsat import *
 from utils.utils import *
 from data.graphDataset import *
 import glob
+from utils.plotter import VisdomLinePlotter
 
 
+plotter = VisdomLinePlotter(env_name="sat")
 
-
-def train(model,dataset,test_split, max_epoch, batch_size=1, print_inter = 10, test_inter = 1, num_workers=0, graph_classif = False, device=torch.device("cuda:0")):
+def train(model,dataset,test_split, max_epoch, lr = 0.01, batch_size=1, print_inter = 10, test_inter = 1, num_workers=0, graph_classif = False, device=torch.device("cuda:0")):
     model.to(device)
     model.train()
 
     train_dl, test_dl, weights = dataset.getDataLoaders(batch_size, test_split, model.maxclause, model.maxvar, model.varvar, graph_classif, num_workers)
 
     #optimizer = torch.optim.AdamW(model.parameters(),amsgrad=True)
-    optimizer = torch.optim.SGD(model.parameters(),lr=0.01)
+    print("using lr: " + str(lr))
+    optimizer = torch.optim.SGD(model.parameters(),lr=lr)
     # class 0 is much more present then others
     class_weights = torch.tensor(weights).to(device)
     criterion = torch.nn.CrossEntropyLoss(weight=class_weights)
@@ -23,9 +25,10 @@ def train(model,dataset,test_split, max_epoch, batch_size=1, print_inter = 10, t
 
     for epoch in range(0, max_epoch):
 
+        model.train()
         for i_batch, sample_batched in enumerate(train_dl):
             batch_size, biggraph, clause_feat, var_feat, graph_pooler, labels = sample_batched
-            target = convert_labels(labels, dataset.neg_as_link, model.maxvar, device)
+            target = convert_labels(labels, dataset.neg_as_link, dataset.maxvar, device)
             biggraph = biggraph.to(device)
             clause_feat = clause_feat.to(device)
             var_feat = var_feat.to(device)
@@ -42,11 +45,15 @@ def train(model,dataset,test_split, max_epoch, batch_size=1, print_inter = 10, t
             loss.backward()
             optimizer.step()
 
+
             running_loss += loss.item()
             if i_batch % print_inter == print_inter-1:    # 2000 / 1999 print every 2000 mini-batches
-                print('[%d, %5d] loss: %.10f' %  (epoch + 1, (i_batch  + 1)*batch_size, running_loss / print_inter))
+                # print('[%d, %5d] loss: %.10f' %  (epoch + 1, (i_batch  + 1)*batch_size, running_loss / print_inter))
+                plotter.plot('loss', 'train', 'Loss', epoch+1, running_loss/print_inter)
+
                 running_loss = 0.0
 
+        model.eval()
         with torch.no_grad():
             precision = []
             precision_nophase = []
@@ -56,58 +63,71 @@ def train(model,dataset,test_split, max_epoch, batch_size=1, print_inter = 10, t
             f1_nophase = []
             for sample_batched in train_dl:
                 batch_size, biggraph, clause_feat, var_feat, graph_pooler, labels = sample_batched
-                target = convert_labels(labels, dataset.neg_as_link, model.maxvar, device)
+                target = convert_labels(labels, dataset.neg_as_link, dataset.maxvar, device)
                 biggraph = biggraph.to(device)
                 clause_feat = clause_feat.to(device)
                 var_feat = var_feat.to(device)
                 out = model.forward(batch_size, biggraph, clause_feat, var_feat, None)
                 res = torch.argmax(out,dim=2)
                 for bi in range(len(labels)):
+                    #print("labels: " + str(labels[bi]))
                     preds = []
-                    for i,r in enumerate(res[bi]):
-                        if r == 1:
-                            preds.append(-(i+1))
-                        elif r == 2:
-                            preds.append(i+1)
-
+                    if dataset.neg_as_link:
+                        for i,r in enumerate(res[bi]):
+                            if r.item() == 1: # neg case
+                                preds.append(-(i+1))
+                            elif r.item() == 2:
+                                preds.append(i+1)
+                    else:
+                        for i,r in enumerate(res[bi]):
+                            if r.item() == 1:
+                                if i >= dataset.maxvar: # neg case
+                                    preds.append(-(i-dataset.maxvar+1))
+                                else:
+                                    preds.append(i+1)
+                    #print("preds: " + str(preds))
                     tp = 0
                     tp_nophase = 0
                     for p in labels[bi]:
-                        if p in preds or -p in preds:
-                            tp_nophase += 1
+                        # if p in preds or -p in preds:
+                        #     tp_nophase += 1
                         if p in preds:
                             tp += 1
                     if len(preds) == 0:
                         local_precision = 0
-                        local_precision_nophase= 0
+                        # local_precision_nophase= 0
                     else:
                         local_precision = tp/len(preds)
-                        local_precision_nophase = tp_nophase/len(preds)
+                        # local_precision_nophase = tp_nophase/len(preds)
 
                     precision.append(local_precision)
-                    precision_nophase.append(local_precision_nophase)
+                    # precision_nophase.append(local_precision_nophase)
                     local_recall = tp / len(labels[bi])
-                    local_recall_nophase = tp_nophase / len(labels[bi])
+                    # local_recall_nophase = tp_nophase / len(labels[bi])
                     recall.append(local_recall)
-                    recall_nophase.append(local_recall_nophase)
+                    # recall_nophase.append(local_recall_nophase)
 
                     if (local_precision + local_recall) == 0:
                         f1.append(0.0)
                     else:
                         f1.append(2 * local_precision * local_recall / (local_precision+local_recall))
 
-                    if (local_precision_nophase + local_recall_nophase) == 0:
-                        f1_nophase.append(0.0)
-                    else:
-                        f1_nophase.append(2 * local_precision_nophase * local_recall_nophase / (local_precision_nophase+local_recall_nophase))
+                    # if (local_precision_nophase + local_recall_nophase) == 0:
+                    #     f1_nophase.append(0.0)
+                    # else:
+                    #     f1_nophase.append(2 * local_precision_nophase * local_recall_nophase / (local_precision_nophase+local_recall_nophase))
 
 
-            print("precision: " + str(sum(precision)/len(precision)))
-            print("recall: " + str(sum(recall)/len(recall)))
-            print("f1: " + str(sum(f1)/len(f1)))
-            print("precision_nophase: " + str(sum(precision_nophase)/len(precision_nophase)))
-            print("recall_nophase: " + str(sum(recall_nophase)/len(recall_nophase)))
-            print("f1_nophase: " + str(sum(f1_nophase)/len(f1_nophase)))
+            plotter.plot('precision', 'train', 'Precision', epoch+1, sum(precision)/len(precision))
+            plotter.plot('recall', 'train', 'Recall', epoch+1, sum(recall)/len(recall))
+            plotter.plot('f1', 'train', 'F1', epoch+1, sum(f1)/len(f1))
+
+            # print("precision: " + str(sum(precision)/len(precision)))
+            # print("recall: " + str(sum(recall)/len(recall)))
+            # print("f1: " + str(sum(f1)/len(f1)))
+            # print("precision_nophase: " + str(sum(precision_nophase)/len(precision_nophase)))
+            # print("recall_nophase: " + str(sum(recall_nophase)/len(recall_nophase)))
+            # print("f1_nophase: " + str(sum(f1_nophase)/len(f1_nophase)))
                     # print("target: " + str(labels[bi]))
                     # print("pred: " + str(preds))
 
@@ -121,15 +141,15 @@ def main():
                         help='number of layers for MLP EXCLUDING the input one (default: 2). 1 means linear model.')
     parser.add_argument('--hidden_dim', type=int, default=8,
                         help='number of hidden units (default: 8)')
-    parser.add_argument('--output_dim', type=int, default=3,
+    parser.add_argument('--output_dim', type=int, default=2,
                         help='output dimension (default: 3 , for true/false/not in clause)')
     parser.add_argument('--final_dropout', type=float, default=0.5,
                         help='final layer dropout for graph embedding (default: 0.5)')
     parser.add_argument('--random', type=int, default=1,
                         help='number of random features (default: 1)')
-    parser.add_argument('--maxclause', type=int, default=6000,
+    parser.add_argument('--maxclause', type=int, default=8000,
                         help='max number of clauses (default: 20M)')
-    parser.add_argument('--maxvar', type=int, default=2000,
+    parser.add_argument('--maxvar', type=int, default=1500,
                         help='max number of vars (default: 5M)')
     parser.add_argument('--graph_type', type=str, default="clause.var",
                         help='graph_type in clause.var|var.var')
@@ -139,8 +159,7 @@ def main():
                         help='goal is clause classification (default: False) can be combined with var classif , both false means whole graph classif')
     parser.add_argument('--pgso', action='store_true',  help='use pgso')
     parser.add_argument('--mpgso', action='store_true',  help='use mpgso')
-    parser.add_argument('--graph_norm', type=bool, default=True,
-                        help='use graph normalization')
+    parser.add_argument('--graph_norm', action='store_true',   help='use graph normalization')
     parser.add_argument('--neighbor_pooling_type', type=str, default="sum", choices=["sum", "average"],                        help='Pooling for over neighboring nodes: sum, average')
     parser.add_argument('--graph_pooling_type', type=str, default="sum", choices=["sum", "average"],
                         help='Pooling for over nodes in a graph: sum or average')
@@ -153,15 +172,21 @@ def main():
     parser.add_argument('--test_split', default = 0.1, help='test split')
     parser.add_argument('--batch_size', type = int, default = 1, help='batch_size')
     parser.add_argument('--permute_vars', action='store_true', help='do random permutation of vars')
+    parser.add_argument('--lr', type=float, default = 0.01,  help='lr')
 
     args = parser.parse_args()
 
-    model = GraphCNNSAT(args.num_layers, args.num_mlp_layers,  args.hidden_dim, args.output_dim, args.final_dropout, args.random, args.maxclause, args.maxvar, args.graph_type, args.var_classif, args.clause_classif, False, args.pgso, args.mpgso, args.graph_norm, args.neighbor_pooling_type, args.graph_pooling_type, args.lfa)
+    tds = GraphDataset(glob.glob(args.graphfile), args.maxvar, args.permute_vars, cachesize=0, path_prefix=args.datasetpath)
+    if tds.neg_as_link:
+        mmv = args.maxvar
+    else:
+        mmv = 2*args.maxvar
+
+    model = GraphCNNSAT(args.num_layers, args.num_mlp_layers,  args.hidden_dim, args.output_dim, args.final_dropout, args.random, args.maxclause, mmv, args.graph_type, args.var_classif, args.clause_classif, False, args.pgso, args.mpgso, args.graph_norm, args.neighbor_pooling_type, args.graph_pooling_type, args.lfa)
     model.to(torch.device("cuda:0"))
 
-    tds = GraphDataset(glob.glob(args.graphfile), args.permute_vars, cachesize=0, path_prefix=args.datasetpath)
 
-    train(model, tds, args.test_split,  args.epoch, args.batch_size)
+    train(model, tds, args.test_split,  args.epoch, args.lr, args.batch_size)
 
 
 if __name__ == '__main__':
